2793abc2bdfce19ee2cc62d62ddca89b998331b4
Sajanpreet35 <sajansingh2037@gmail.com>
2025-08-28 17:14:16 +0530

    Create Main.Activity.kt

---
+++ Main.Activity.kt
@@ -0,0 +1,17 @@
+package com.example.studydiscussionclub
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startButton: Button = findViewById(R.id.startButton)
+        startButton.setOnClickListener {
+            // Temporary action, abhi kuch nahi
+        }
+    }
+}
